16ae9db8fe1b63f0dd772510bce728f111ee370d
Waynexia888 <qiwuwaynexia@gmail.com>
2020-09-05 23:38:16 -0700

    connected mongoose to database

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./confg/keys").mongoURI;
+
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log(err));
+
+
 
 app.get("/", (req, res) => {
     res.send("Hello Wayne!")
