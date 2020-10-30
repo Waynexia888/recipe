571fb222b046129bdab0214f8e32837e862b18a3
Waynexia888 <qiwuwaynexia@gmail.com>
2020-10-29 23:30:03 -0700

    added validation in users.js routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const User = require('../../models/User');
 const bcrypt = require("bcryptjs");
 const keys = require("../../confg/keys");
 const jwt = require("jsonwebtoken");
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 router.get("/test", (req, res) => {
     res.json({ msg: "This is the user route"});
@@ -11,6 +13,12 @@ router.get("/test", (req, res) => {
 
 
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    
     User.findOne({ email: req.body.email })
     .then(user => {
         if (user) {
